0476f80a0d6948027d84f528d3ed18afdded8aba
NullaOfficial <davidwangwu104@gmail.com>
2026-02-25 20:40:50 -0400

    Refactor command sending to use byte array
    Updated the command sending mechanism to use a byte array instead of a formatted string. This change improves the way commands are constructed and sent over the serial connection.

--- src/comunicacion_serial.py
+++ src/comunicacion_serial.py
@@ -4,19 +4,19 @@ import time
 ser = serial.Serial('COM3', 115200, timeout = 1)
 time.sleep(2) 
 
-#header = 0xFF   
+header = 0xFF   
 tipo_de_comando = 1 
 accion = 1   
 arg1 = 100      
 arg2 = 100      
 
 
-comando_txt = f"{tipo_de_comando}{accion}{arg1}{arg2}\n"
-#comando = bytes([header, tipo_de_comando, accion, arg1, arg2])
+#comando_txt = f"{tipo_de_comando}{accion}{arg1}{arg2}\n"
+comando = bytes([header, tipo_de_comando, accion, arg1, arg2])
 
-#ser.write(comando)
-#print("Comando:", comando.hex())
-ser.write(comando_txt.encode('utf-8'))
-print(f"Enviado: {comando_txt.strip()}")
+ser.write(comando)
+print("Comando:", comando.hex())
+#ser.write(comando_txt.encode('utf-8'))
+#print(f"Enviado: {comando_txt.strip()}")
 
 ser.close()
