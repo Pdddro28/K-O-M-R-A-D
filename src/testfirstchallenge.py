2c4542b5fc0976488c8364225acb5a01f566f61d
Pedro Catamo <pedrocatamo.2009@gmail.com>
2026-04-24 18:32:53 -0400

    Feat:Test First Challenge Created

--- src/testfirstchallenge.py
+++ src/testfirstchallenge.py
@@ -23,15 +23,14 @@ while running:
     try:
         LNM.move_forward(speed=100)  # Works
         # get areas and contours-----------------
-        LNM.vision.receive_image()
-        
-        cnt_lines_blue = LNM.vision.find_contours(mask_blue_test, ROI_LINES)
-        cnt_lines_orange = LNM.vision.find_contours(mask_orange_test, ROI_LINES)
-        cnt_front_wall = LNM.vision.find_contours(mask_black_test, OPEN_ROI_CENTER)
-        
-        black_area = LNM.vision.max_contour(cnt_front_wall, OPEN_ROI_CENTER)[0]
-        blue_area = LNM.vision.max_contour(cnt_lines_blue, ROI_LINES)[0]
-        orange_area = LNM.vision.max_contour(cnt_lines_orange, ROI_LINES)[0]
+        #LNM.vision.receive_image()
+        front_dist, left_dist, right_dist = LNM.get_distances()
+        print(f"Distances - Front: {front_dist} | Left: {left_dist} | Right: {right_dist}")
+
+        if front_dist < 20:
+            print("Obstacle detected! Stopping.")
+            LNM.stop()
+            break
         # get areas and contours-----------------
         
         # get turn direction based on line color----------
@@ -64,29 +63,6 @@ while running:
         print(loops)
         
         
-        
-        #DRAWING------------------------------------------------------
-        #draw rois---------s
-        for roi in ROIS:
-            LNM.vision.draw_roi(roi)
-        #draw rois---------
-            
-        #draw contours-----------
-        LNM.vision.draw_contours(cnt_front_wall, OPEN_ROI_CENTER, (255,255,0))
-        LNM.vision.draw_contours(cnt_lines_blue, ROI_LINES, (255,255,0))
-        LNM.vision.draw_contours(cnt_lines_orange, ROI_LINES, (255,0,0))
-        #draw contours-----------
-        
-        #draw final img
-        cv.putText(LNM.vision.frame, "black area: " + str(black_area), (0,20), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-        cv.putText(LNM.vision.frame, "blue area: " + str(blue_area), (0,40), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-        cv.putText(LNM.vision.frame, "orange area: " + str(orange_area), (0,60), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-        cv.imshow("frame", LNM.vision.frame)
-        
-        #------------
-        if cv.waitKey(1) == ord('q') or not running:
-            LNM.vision.camera_cap.release()
-            cv.destroyAllWindows()
             
         
     except Exception as e:
